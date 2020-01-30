2af1cdbc29fa4408aa0a85e7a65382ee96aef1ab
Wisdom Tohe <elmanciowisdom@gmail.com>
2020-01-30 07:30:02 +0000

    ajout des fichiers du projet sur le depot distant

---
+++ src/main/java/com/fuegoleon/gl_vue_api/GlVueApiApplication.java
@@ -0,0 +1,13 @@
+package com.fuegoleon.gl_vue_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlVueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlVueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuegoleon/gl_vue_api/GlVueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuegoleon.gl_vue_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlVueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
